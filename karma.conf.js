53f5f6ca0e2b362706b3d4862fb9240b57b48201
Borewit <Borewit@users.noreply.github.com>
2021-08-02 08:49:49 +0200

    Update to WebPack v5
    Get rid of `node:assert` dependency, use `expect` instead.

--- karma.conf.js
+++ karma.conf.js
@@ -2,54 +2,60 @@
 // https://karma-runner.github.io/1.0/config/configuration-file.html
 
 const path = require('path');
+const webpack = require('webpack');
 
 module.exports = config => {
   config.set({
-    basePath: '.',
+    basePath: 'lib',
     frameworks: [
       'jasmine'
     ],
     files: [
-      {pattern: 'lib/**/*.spec.ts'}
+      {pattern: '**/*.spec.ts'}
     ],
     preprocessors: {
-      '**/*.ts': 'webpack',
+      '**/*.ts': 'webpack'
     },
 
     webpack: {
       mode: 'development',
-      entry: './lib/index.ts',
       resolve: {
-        extensions: ['.tsx', '.ts', '.js']
+        extensions: ['.tsx', '.ts', '.js'],
+        fallback: {'buffer': require.resolve('buffer/')}
       },
+      // Ensure buffer is available
+      plugins: [
+        new webpack.ProvidePlugin({
+          process: 'process/browser',
+          Buffer: ['buffer', 'Buffer']
+        })
+      ],
       devtool: 'inline-source-map',
       module: {
         rules: [
           {
             test: /\.ts$/,
-            use: 'ts-loader',
-            include: path.resolve('lib')
+            use: 'ts-loader'
           },
           {
             test: /\.ts$/,
             use: {loader: 'istanbul-instrumenter-loader'},
             enforce: 'post',
-            include: path.resolve('lib'),
             exclude: /\.spec\.ts$/
           }
 
         ]
-      },
+      }
     },
     webpackMiddleware: {
       noInfo: true
     },
 
-    reporters: ['progress', 'kjhtml', 'coverage-istanbul', 'spec'],
+    reporters: ['kjhtml', 'dots', 'coverage-istanbul', 'BrowserStack'],
     // https://www.npmjs.com/package/karma-coverage-istanbul-reporter
     coverageIstanbulReporter: {
       dir: path.join(__dirname, 'coverage'),
-      reports: ['text-summary', 'lcovonly', 'html'],
+      reports: ['text-summary', 'lcovonly'],
       fixWebpackSourcePaths: true,
       'report-config': {
         html: {
@@ -63,44 +69,55 @@ module.exports = config => {
     // global BrowserStack configuration
     browserStack: {
       forcelocal: true,  // force traffic through the local BrowserStack tunnel, passes flag through to BrowserStackTunnel
-      project: 'readable-web-to-node-stream'
+      project: 'readable-web-to-node-stream',
+      timeout: 30,  // BROWSERSTACK_IDLE_TIMEOUT
+      captureTimeout: 30
     },
 
-    // define browsers
+    // define browsers, see https://www.browserstack.com/automate/capabilities
     customLaunchers: {
       bs_win_chrome: {
         base: 'BrowserStack',
         os: 'Windows',
         os_version: '10',
-        browser: 'Chrome'
+        browser: 'Chrome',
+        browser_version: '88.0'
       },
       bs_win_firefox: {
         base: 'BrowserStack',
         os: 'Windows',
         os_version: '10',
-        browser: 'Firefox'
+        browser: 'Firefox',
+        browser_version: '84.0'
       },
       bs_osx_safari: {
         base: 'BrowserStack',
         os: 'OS X',
-        os_version: 'Mojave',
-        browser: 'Safari'
+        os_version: 'Big Sur',
+        browser: 'Safari',
+        browser_version: '14'
       },
       bs_win_edge: {
         base: 'BrowserStack',
         os: 'Windows',
         os_version: '10',
-        browser: 'Edge'
+        browser: 'Edge',
+        browser_version: '88'
       }
     },
 
-    //autoWatch: true,
-    browsers: ['Chrome', 'Safari', 'Firefox'],
-    colors: true,
-    singleRun: false,
+    mocha: {
+      timeout: 20000 // 20 seconds
+    },
 
-    browserNoActivityTimeout: 20000,
-    browserDisconnectTimeout: 30000,
-    browserDisconnectTolerance: 1,
+    //autoWatch: true,
+    browsers: ['Chrome'],
+    colors: true,
+
+    // Increase time-outs to prevent disconnects on BrowserStack
+    browserDisconnectTimeout: 10000, // default 2000
+    browserDisconnectTolerance: 1, // default 0
+    browserNoActivityTimeout: 4 * 60 * 1000, //default 10000
+    captureTimeout: 4 * 60 * 1000 //default 60000
   });
 };
